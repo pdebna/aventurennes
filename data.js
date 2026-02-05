b723d3de8ec34126b49f829b59ea0aaf056b8428
pdebna <87902826+pdebna@users.noreply.github.com>
2026-02-05 22:38:31 +0100

    Update data.js

--- data.js
+++ data.js
@@ -5,7 +5,7 @@ const enigmes = [
     question: "QUESTION 1",
     answers: ["reponse 1"],
     stops: ["ARRET A", "ARRET B", "ARRET C"],
-    letter: "V"
+    letter: "V",
     message: "Eh bien, ça en fait des colonnes !"
   },
 
@@ -14,7 +14,7 @@ const enigmes = [
     question: "QUESTION 2",
     answers: ["reponse 2"],
     stops: ["ARRET D", "ARRET E", "ARRET F"],
-    letter: "E"
+    letter: "E",
     message: "Eh bien, ça en fait des colonnes !"
   },
 
@@ -23,7 +23,7 @@ const enigmes = [
     question: "QUESTION 3",
     answers: ["reponse 3"],
     stops: ["ARRET G", "ARRET H", "ARRET I"],
-    letter: "U"
+    letter: "U",
     message: "Eh bien, ça en fait des colonnes !"
   },
 
@@ -32,7 +32,7 @@ const enigmes = [
     question: "QUESTION 4",
     answers: ["reponse 4"],
     stops: ["ARRET J", "ARRET K", "ARRET L"],
-    letter: "X"
+    letter: "X",
     message: "Eh bien, ça en fait des colonnes !"
   },
 
@@ -41,7 +41,7 @@ const enigmes = [
     question: "QUESTION 5",
     answers: ["reponse 5"],
     stops: ["ARRET M", "ARRET N", "ARRET O"],
-    letter: "T"
+    letter: "T",
     message: "Eh bien, ça en fait des colonnes !"
   },
 
@@ -50,7 +50,7 @@ const enigmes = [
     question: "QUESTION 6",
     answers: ["reponse 6"],
     stops: ["ARRET P", "ARRET Q", "ARRET R"],
-    letter: "U"
+    letter: "U",
     message: "Eh bien, ça en fait des colonnes !"
   },
 
@@ -59,7 +59,7 @@ const enigmes = [
     question: "QUESTION 7",
     answers: ["reponse 7"],
     stops: ["ARRET S", "ARRET T", "ARRET U"],
-    letter: "E"
+    letter: "E",
     message: "Eh bien, ça en fait des colonnes !"
   },
 
@@ -68,7 +68,7 @@ const enigmes = [
     question: "QUESTION 8",
     answers: ["reponse 8"],
     stops: ["ARRET V", "ARRET W", "ARRET X"],
-    letter: "T"
+    letter: "T",
     message: "Eh bien, ça en fait des colonnes !"
   },
 
@@ -77,7 +77,7 @@ const enigmes = [
     question: "QUESTION 9",
     answers: ["reponse 9"],
     stops: ["ARRET Y", "ARRET Z", "ARRET A1"],
-    letter: "R"
+    letter: "R",
     message: "Eh bien, ça en fait des colonnes !"
   },
 
@@ -86,7 +86,7 @@ const enigmes = [
     question: "QUESTION 10",
     answers: ["reponse 10"],
     stops: ["ARRET B1", "ARRET C1", "ARRET D1"],
-    letter: "E"
+    letter: "E",
     message: "Eh bien, ça en fait des colonnes !"
   },
 
@@ -95,7 +95,7 @@ const enigmes = [
     question: "QUESTION 11",
     answers: ["reponse 11"],
     stops: ["ARRET E1", "ARRET F1", "ARRET G1"],
-    letter: "T"
+    letter: "T",
     message: "Eh bien, ça en fait des colonnes !"
   },
 
@@ -104,7 +104,7 @@ const enigmes = [
     question: "QUESTION 12",
     answers: ["reponse 12"],
     stops: ["ARRET H1", "ARRET I1", "ARRET J1"],
-    letter: "E"
+    letter: "E",
     message: "Eh bien, ça en fait des colonnes !"
   },
 
@@ -113,7 +113,7 @@ const enigmes = [
     question: "QUESTION 13",
     answers: ["reponse 13"],
     stops: ["ARRET K1", "ARRET L1", "ARRET M1"],
-    letter: "M"
+    letter: "M",
     message: "Eh bien, ça en fait des colonnes !"
   },
 
@@ -122,7 +122,7 @@ const enigmes = [
     question: "QUESTION 14",
     answers: ["reponse 14"],
     stops: ["ARRET N1", "ARRET O1", "ARRET P1"],
-    letter: "O"
+    letter: "O",
     message: "Eh bien, ça en fait des colonnes !"
   },
 
@@ -131,7 +131,7 @@ const enigmes = [
     question: "QUESTION 15",
     answers: ["reponse 15"],
     stops: ["ARRET Q1", "ARRET R1", "ARRET S1"],
-    letter: "I"
+    letter: "I",
     message: "Eh bien, ça en fait des colonnes !"
   },
 
@@ -140,7 +140,7 @@ const enigmes = [
     question: "QUESTION 16",
     answers: ["reponse 16"],
     stops: ["ARRET T1", "ARRET U1", "ARRET V1"],
-    letter: "N"
+    letter: "N",
     message: "Eh bien, ça en fait des colonnes !"
   }
 

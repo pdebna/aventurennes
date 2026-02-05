77bdcecdbbd86ec0e3831e6828b105fb75f02842
pdebna <87902826+pdebna@users.noreply.github.com>
2026-02-05 16:51:13 +0100

    Create data.js

---
+++ data.js
@@ -0,0 +1,131 @@
+const enigmes = [
+
+  {
+    id: 1,
+    question: "QUESTION 1",
+    answers: ["reponse 1"],
+    stops: ["ARRET A", "ARRET B", "ARRET C"],
+    letter: "V"
+  },
+
+  {
+    id: 2,
+    question: "QUESTION 2",
+    answers: ["reponse 2"],
+    stops: ["ARRET D", "ARRET E", "ARRET F"],
+    letter: "E"
+  },
+
+  {
+    id: 3,
+    question: "QUESTION 3",
+    answers: ["reponse 3"],
+    stops: ["ARRET G", "ARRET H", "ARRET I"],
+    letter: "U"
+  },
+
+  {
+    id: 4,
+    question: "QUESTION 4",
+    answers: ["reponse 4"],
+    stops: ["ARRET J", "ARRET K", "ARRET L"],
+    letter: "X"
+  },
+
+  {
+    id: 5,
+    question: "QUESTION 5",
+    answers: ["reponse 5"],
+    stops: ["ARRET M", "ARRET N", "ARRET O"],
+    letter: "T"
+  },
+
+  {
+    id: 6,
+    question: "QUESTION 6",
+    answers: ["reponse 6"],
+    stops: ["ARRET P", "ARRET Q", "ARRET R"],
+    letter: "U"
+  },
+
+  {
+    id: 7,
+    question: "QUESTION 7",
+    answers: ["reponse 7"],
+    stops: ["ARRET S", "ARRET T", "ARRET U"],
+    letter: "E"
+  },
+
+  {
+    id: 8,
+    question: "QUESTION 8",
+    answers: ["reponse 8"],
+    stops: ["ARRET V", "ARRET W", "ARRET X"],
+    letter: "T"
+  },
+
+  {
+    id: 9,
+    question: "QUESTION 9",
+    answers: ["reponse 9"],
+    stops: ["ARRET Y", "ARRET Z", "ARRET A1"],
+    letter: "R"
+  },
+
+  {
+    id: 10,
+    question: "QUESTION 10",
+    answers: ["reponse 10"],
+    stops: ["ARRET B1", "ARRET C1", "ARRET D1"],
+    letter: "E"
+  },
+
+  {
+    id: 11,
+    question: "QUESTION 11",
+    answers: ["reponse 11"],
+    stops: ["ARRET E1", "ARRET F1", "ARRET G1"],
+    letter: "T"
+  },
+
+  {
+    id: 12,
+    question: "QUESTION 12",
+    answers: ["reponse 12"],
+    stops: ["ARRET H1", "ARRET I1", "ARRET J1"],
+    letter: "E"
+  },
+
+  {
+    id: 13,
+    question: "QUESTION 13",
+    answers: ["reponse 13"],
+    stops: ["ARRET K1", "ARRET L1", "ARRET M1"],
+    letter: "M"
+  },
+
+  {
+    id: 14,
+    question: "QUESTION 14",
+    answers: ["reponse 14"],
+    stops: ["ARRET N1", "ARRET O1", "ARRET P1"],
+    letter: "O"
+  },
+
+  {
+    id: 15,
+    question: "QUESTION 15",
+    answers: ["reponse 15"],
+    stops: ["ARRET Q1", "ARRET R1", "ARRET S1"],
+    letter: "I"
+  },
+
+  {
+    id: 16,
+    question: "QUESTION 16",
+    answers: ["reponse 16"],
+    stops: ["ARRET T1", "ARRET U1", "ARRET V1"],
+    letter: "N"
+  }
+
+];
